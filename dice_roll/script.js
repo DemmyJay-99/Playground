7cdc46fcb1c651608219b48888bd53b51e42a886
DemmyJay <demmyjyd@gmail.com>
2025-06-06 02:58:02 +0100

    Create dice_roll

--- dice_roll/script.js
+++ dice_roll/script.js
@@ -1,8 +1,21 @@
-const roll = document.getElementById('roll');
-const label = document.getElementById('label');
-max = 10;
-min = 1;
+    const roll = document.getElementById('roll');
 
-roll.onmouseover= function(){
-    label.textContent = Math.floor(Math.random() * max );
+
+roll.onclick = function(){
+    const label = document.getElementById('label');
+    const input = document.getElementById('input').value;
+    const diceResult = document.getElementById('diceresult');
+    const diceImages = document.getElementById('diceimages');
+
+    const values = [];
+    const images = [];
+
+    for (let i =0; i < input; i++) {
+            const value = Math.floor(Math.random() * 6) + 1;
+            values.push(value);
+            images.push(` <img src="dice/${value}.png" alt= "N">`);
+    }
+
+    diceResult.textContent = values.join(",");
+    diceImages.innerHTML = images.join(" ");
 }
