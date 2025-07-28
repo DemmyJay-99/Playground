7878b8853895d2d133282c24b0e6f0952e0de947
DemmyJay <demmyjyd@gmail.com>
2025-07-28 22:13:58 +0100

    Add TAN button

--- calculator/script.js
+++ calculator/script.js
@@ -25,6 +25,11 @@ function calculateCos(){
     const input = parseFloat(display.value);
     display.value = Math.cos(input);
 };
+
+function calculateTan(){
+    const input = parseFloat(display.value);
+    display.value = Math.tan(input);
+};
 // const allowedKeys = ['0','1','2','3','4','5','6','7','8','9','+','-','*','/','.'];
 
 // <!DOCTYPE html>
