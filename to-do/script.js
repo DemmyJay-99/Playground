d6e93022ebb850dd49994ee9451358bf20066770
DemmyJay <demmyjyd@gmail.com>
2025-08-03 13:39:46 +0100

    Add remove item button

--- to-do/script.js
+++ to-do/script.js
@@ -5,8 +5,13 @@ const content = document.getElementById('content');
 function addItem() {
     if(input.value) {
     const listItem = document.createElement('li')
-    listItem.innerHTML = `<input type="checkbox"> ${input.value} <i class="fa-solid fa-xmark"></i>`;
+    listItem.innerHTML = `<input type="checkbox"> ${input.value} <i class="fa-solid fa-xmark" onclick="deleteItem()"></i>`;
 
     content.appendChild(listItem);
+    listItem.querySelector('i').addEventListener('click', () =>{
+        listItem.remove();
+    }
+
+    )
     }
 }
