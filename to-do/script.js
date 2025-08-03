95c78231605d2e5bc74aeb896d54febf15576e64
DemmyJay <demmyjyd@gmail.com>
2025-08-03 13:47:44 +0100

    Add checkbox styling

--- to-do/script.js
+++ to-do/script.js
@@ -5,13 +5,15 @@ const content = document.getElementById('content');
 function addItem() {
     if(input.value) {
     const listItem = document.createElement('li')
-    listItem.innerHTML = `<input type="checkbox"> ${input.value} <i class="fa-solid fa-xmark" onclick="deleteItem()"></i>`;
+    listItem.innerHTML = `<input type="checkbox" class="check"> ${input.value} <i class="fa-solid fa-xmark" onclick="deleteItem()"></i>`;
 
     content.appendChild(listItem);
     listItem.querySelector('i').addEventListener('click', () =>{
         listItem.remove();
-    }
+    })
 
-    )
+    listItem.querySelector('.check').addEventListener('change', () =>{
+        listItem.classList.toggle('done');
+    })
     }
 }
