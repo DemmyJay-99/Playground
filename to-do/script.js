a85bf56c135d18a0854cf635b9f95abc5809cfa4
DemmyJay <demmyjyd@gmail.com>
2025-08-03 13:12:46 +0100

    Add checkbox and x

--- to-do/script.js
+++ to-do/script.js
@@ -3,8 +3,10 @@ const btn = document.getElementById('btn');
 const content = document.getElementById('content');
 
 function addItem() {
+    if(input.value) {
     const listItem = document.createElement('li')
-    listItem.innerHTML = `${input.value}`;
+    listItem.innerHTML = `<input type="checkbox"> ${input.value} <i class="fa-solid fa-xmark"></i>`;
 
     content.appendChild(listItem);
+    }
 }
